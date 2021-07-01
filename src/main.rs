172b9863f56cf72a62c4a91b73b32e1dfb342e99
marius david <mariusdavid@laposte.net>
2021-07-01 11:57:25 +0200

    added a lot of new feature, need cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use clap::Clap;
-use std::str::FromStr;
 use anyhow::{Context, Result};
-use pmd_dic::{KandChar, KandFile};
+use clap::Clap;
+use fontdue::{Font, FontSettings};
+use image::{DynamicImage, GenericImage, ImageBuffer, ImageFormat, LumaA, Rgba};
 use pmd_cte::{CteFormat, CteImage};
-use image::{DynamicImage, GenericImage, ImageBuffer, Rgba};
+use pmd_dic::{KandChar, KandFile};
+use std::path::Path;
+use std::{fs::DirBuilder, io::Read, str::FromStr};
 use std::{
     fs::{create_dir_all, read_dir, File},
     path::PathBuf,
@@ -22,7 +24,10 @@ enum SubCommand {
     Generate(GenerateParameter),
     /// Read a folder in the format extracted by generate, and write it to a .dic and a .img
     Build(BuildParameter),
+    /// Read a truetype font, and export a folder that can be read by the build command
+    FromTruetype(FromTruetypeParameter),
 }
+
 #[derive(Clap)]
 pub struct GenerateParameter {
     /// the input .dic file
@@ -43,11 +48,22 @@ pub struct BuildParameter {
     img_output: PathBuf,
 }
 
+#[derive(Clap)]
+pub struct FromTruetypeParameter {
+    /// the input TrueType font
+    input: PathBuf,
+    /// the output folder
+    output: PathBuf,
+}
+
 fn main() -> Result<()> {
     let opts = Opts::parse();
     match opts.subcmd {
         SubCommand::Generate(gp) => generate(gp).context("can't generate the glyphs")?,
         SubCommand::Build(bp) => build(bp).context("can't build the .img and .dic file")?,
+        SubCommand::FromTruetype(fp) => {
+            from_truetype(fp).context("can't generate the font result from the TrueType font")?
+        }
     };
     Ok(())
 }
@@ -56,8 +72,8 @@ pub struct CharData {
     pub char: u16,
     pub glyth_width: u16,
     pub glyth_height: u16,
-    pub unk1: u16,
-    pub unk2: u16,
+    pub unk1: i16, //TODO: seems to be xmin
+    pub unk2: i16, //TODO: seems to by ymin
     pub distance: u16,
     pub unk4: u16,
     pub unk5: u16,
@@ -81,7 +97,7 @@ fn generate(gp: GenerateParameter) -> Result<()> {
         );
         let file_name = format!(
             "{}_{}_{}_{}_{}_{}.png",
-            char.char, char.unk1, char.unk2, char.unk3, char.unk4, char.unk5
+            char.char, char.unk1, char.unk2, char.distance, char.unk4, char.unk5
         );
         let target_file = gp.output.join(file_name);
         section.save(target_file)?;
@@ -104,22 +120,36 @@ fn build(bp: BuildParameter) -> Result<()> {
             .to_string_lossy();
         let mut splited = stem.split('_');
 
-        let mut read_from_splited_u16 = || -> Result<u16> {
-            let section = splited.next().with_context(|| format!("the path {:?} doesn't have the good format of \"charid_unk1_unk2_distance_unk4_unk5.ext\"", char_path))?;
-            Ok(u16::from_str(section).with_context(|| {
+        fn get_text_from_file_name<'a>(iter: &mut impl Iterator<Item = &'a str>, file_path: &Path) -> Result<&'a str> {
+            iter.next().with_context(|| format!("the path {:?} doesn't have the good format of \"charid_unk1_unk2_distance_unk4_unk5.ext\"", file_path))
+        }
+
+        fn read_u16_from_splited<'a>(iter: &mut impl Iterator<Item = &'a str>, file_path: &Path) -> Result<u16>{
+            let text = get_text_from_file_name(iter, file_path)?;
+            Ok(u16::from_str(text).with_context(|| {
                 format!(
-                    "can't transform the text {:?} to a u16 character (for the file name at {:?})",
-                    section, char_path
+                    "can't transform the text {:?} to a u16 number (for the file name at {:?})",
+                    text, file_path
                 )
             })?)
-        };
+        }
 
-        let char_id = read_from_splited_u16()?;
-        let unk1 = read_from_splited_u16()?;
-        let unk2 = read_from_splited_u16()?;
-        let distance = read_from_splited_u16()?;
-        let unk4 = read_from_splited_u16()?;
-        let unk5 = read_from_splited_u16()?;
+        fn read_i16_from_splited<'a>(iter: &mut impl Iterator<Item = &'a str>, file_path: &Path) -> Result<i16>{
+            let text = get_text_from_file_name(iter, file_path)?;
+            Ok(i16::from_str(text).with_context(|| {
+                format!(
+                    "can't transform the text {:?} to a i16 number (for the file name at {:?})",
+                    text, file_path
+                )
+            })?)
+        }
+
+        let char_id = read_u16_from_splited(&mut splited, &char_path)?;
+        let unk1 = read_i16_from_splited(&mut splited, &char_path)?;
+        let unk2 = read_i16_from_splited(&mut splited, &char_path)?;
+        let distance = read_u16_from_splited(&mut splited, &char_path)?;
+        let unk4 = read_u16_from_splited(&mut splited, &char_path)?;
+        let unk5 = read_u16_from_splited(&mut splited, &char_path)?;
 
         let char_image = image::io::Reader::open(char_path)?.decode()?.to_rgba8();
         //TODO: what if they can't be transformed to as u16 ?
@@ -171,7 +201,7 @@ fn build(bp: BuildParameter) -> Result<()> {
             glyth_height: char_data.glyth_height,
             unk1: char_data.unk1,
             unk2: char_data.unk2,
-            unk3: char_data.distance,
+            distance: char_data.distance,
             unk4: char_data.unk4,
             unk5: char_data.unk5,
         };
@@ -208,5 +238,63 @@ fn build(bp: BuildParameter) -> Result<()> {
     let mut cte_writer = File::create(bp.img_output)?;
     cte_image.encode_cte(&mut cte_writer)?;
     println!("done");
+    Ok(())
+}
+
+pub fn from_truetype(fp: FromTruetypeParameter) -> Result<()> {
+    DirBuilder::new()
+        .recursive(true)
+        .create(&fp.output)
+        .with_context(|| format!("can't create the target directory {:?}", fp.output))?;
+
+    let scale = 14; //TODO: allow the user to change this value
+
+    let mut ttf_file =
+        File::open(&fp.input).with_context(|| format!("can't open the file at {:?}", fp.input))?;
+    let mut ttf_bytes = Vec::new();
+    ttf_file.read_to_end(&mut ttf_bytes).with_context(|| {
+        format!(
+            "can't read the complete content of the file at {:?}",
+            fp.input
+        )
+    })?;
+    let ttf_font = Font::from_bytes(
+        ttf_bytes,
+        FontSettings {
+            scale: scale as f32,
+            ..Default::default()
+        },
+    )
+    .unwrap(); //TODO: make it work with anyhow
+
+    // TODO: allow the user to select this list manually... Or export all chars from the font file...
+
+    let chars_to_include = &[
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        '(', ')', '.', '\'', '`', '"', '€', 'ŧ'
+    ];
+
+    for char in chars_to_include {
+        println!("rasterizing {:?}", chars_to_include);
+        let (metric, bitmap_luminance) = ttf_font.rasterize(*char, scale as f32);
+        let mut bitmap: Vec<u8> = Vec::new();
+        for pixel in bitmap_luminance.into_iter() {
+            bitmap.push(0);
+            bitmap.push(pixel);
+        };
+        let char_image: ImageBuffer<LumaA<u8>, Vec<_>> =
+            ImageBuffer::from_vec(metric.width as u32, metric.height as u32, bitmap)
+                .with_context(|| format!("can't read the decoded character {:?}", char))?;
+        //TODO: better parameter
+        let file_name = format!("{}_{}_{}_{}_10_10.png", *char as u16, metric.xmin as i16, -metric.ymin as i16 + scale as i16 - metric.height as i16, metric.advance_width as i16);
+        let mut out_char_path = fp.output.clone();
+        out_char_path.push(file_name);
+        char_image
+            .save_with_format(&out_char_path, ImageFormat::Png)
+            .with_context(|| format!("can't create/encode the image at {:?}", out_char_path))?;
+    }
+
     Ok(())
 }
